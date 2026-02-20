248e971ea51992bdb4fc6951365f1f25294b33f7
Rudy Serratio <rudy.chcho@gmail.com>
2026-02-19 23:56:42 -0800

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Chacho on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
