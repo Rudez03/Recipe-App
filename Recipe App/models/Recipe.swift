d88436cbab58aff1f07af77b746b020e13008d0c
Rudy Serrato <Rudy.chacho@gmail.com>
2026-03-25 15:13:03 -0700

    started Recipe, Recipe file

---
+++ Recipe App/models/Recipe.swift
@@ -0,0 +1,26 @@
+//
+//  Recipe.swift
+//  Recipe App
+//
+//  Created by Rudy Serrato III on 3/25/26.
+//
+
+import Foundation
+
+class Recipe: Identifiable {
+	let id = UUID()
+	
+	var name: String
+	var description: String
+	var ingredients: [String]
+	var prepTime: String
+	
+	var icon: String?
+	
+	init(name: String) {
+		self.name = name
+		self.description = ""
+		self.ingredients = []
+		self.prepTime = ""
+	}
+}
