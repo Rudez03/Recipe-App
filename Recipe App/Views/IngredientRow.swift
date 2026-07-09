76303f89ccd18bf534278359b2d3ca5acdb69000
Rudy Serrato <Rudy.chacho@gmail.com>
2026-07-08 23:28:56 -0700

    Created Ingredient View file did not work on it

---
+++ Recipe App/Views/IngredientRow.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientRow.swift
+//  Recipe App
+//
+//  Created by Chacho on 7/8/26.
+//
+
+import SwiftUI
+
+struct IngredientRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientRow()
+}
